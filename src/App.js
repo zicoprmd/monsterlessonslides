d255898ecbd4779ac3daf4a76ba4c87ef050eeb6
zicoprmd <zicoprmd@gmail.com>
2022-10-10 19:30:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageSlider from './ImageSlider';
+
+import zema1 from './assets/IMG_9742.JPG';
 
 function App() {
+  const slides = [
+    { url: zema1, title: 'z1' },
+    { url: 'http://localhost:3000/z2.JPG', title: 'z2' },
+    { url: 'http://localhost:3000/z3.JPG', title: 'z3' },
+    { url: 'http://localhost:3000/z4.JPG', title: 'z4' },
+    { url: 'http://localhost:3000/z5.JPG', title: 'z5' },
+  ];
+  const containerStyles = {
+    width: '500px',
+    height: '280px',
+    margin: '0 auto',
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello monsterlessons</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
 }
